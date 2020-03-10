bbe4728cb0f284e95aaf54db91b2cb7c949e4caa
Charles Butler <cdbutle@outlook.com>
2020-03-10 01:54:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ array = ["hi","hello","hey","yo"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("sup")
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
